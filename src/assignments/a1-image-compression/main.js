3bf0fa676e91762e45b3e4f432b749b070a93841
klewka <169600284+klewka@users.noreply.github.com>
2026-08-31 17:40:03 -0600

    begin block averages

--- src/assignments/a1-image-compression/main.js
+++ src/assignments/a1-image-compression/main.js
@@ -1,6 +1,7 @@
 import photoUrl from "./my-photo.jpg"
 import { imageDataFromSource } from "@/lib/image/loadImage.js";
 import { createSampleImage } from "@/lib/image/sampleImage.js";
+import { averageBlocks } from "@/lib/image/blockAverage.js";
 
 const IMAGE_SIZE = 320 // size in px
 
@@ -25,31 +26,30 @@ function onControlChanged(name, newValue) {
     const label = document.getElementById(name+"Label");
     label.innerHTML = controlLabels[name] + " " + newValue;
     controlValues[name] = newValue;
+
+    const controlChangedEvent = new CustomEvent("controlChanged", {
+        detail: {
+            control: name,
+            value: newValue
+        }});
+
+    const canvases = document.querySelectorAll('canvas')
+    canvases.forEach((canvas) => canvas.dispatchEvent(controlChangedEvent));
+
+
 }
 //#endregion
 
-let sampleCanvas = null;
-let personalCanvas = null;
-
-function buildPersonalImage() {
-    let image = null;
-    let error = null;
-    fetch(photoUrl).then(
-        response => {
-            let bitmap = createImageBitmap(response.blob);
-            image = imageDataFromSource(bitmap)
-        },
-        reason => {
-            error = reason;
-        }
-    );
-    // CATCH FETCH ERROR
-
-    if (error != null) {
-
+async function getPersonalImage() {
+    try {
+        const response = await fetch(photoUrl);
+        const data = await response.blob();
+        const bitmap = await createImageBitmap(data);
+        return imageDataFromSource(bitmap, IMAGE_SIZE);
+    } catch (error) {
+        console.error("Failed to get personal image:", error.message);
+        return error.message;
     }
-
-    return image;
 }
 
 const IMAGE_MODIFIERS = [
@@ -57,7 +57,8 @@ const IMAGE_MODIFIERS = [
         title: "Original",
         fn: function(_imageData) {
             return _imageData;
-        }
+        },
+        showDownload: true
     },
     {
         title: "Indexed Color",
@@ -77,64 +78,114 @@ const IMAGE_MODIFIERS = [
         title: "Block Average",
         control: "blockSize",
         fn: function(_imageData) {
-            return _imageData;
+            return averageBlocks(_imageData, controlValues["blockSize"]);
         }
     }
 ]
 
-function buildGallery() {
-    let sampleContainer = document.getElementById("sample-container");
-
-    let gallery = document.createElement("div");
-    gallery.id = "sampleGallery";
-    gallery.classList.add("gallery");
-
+function getSampleImage() {
     const sampleImage = createSampleImage(IMAGE_SIZE);
-    const imageData = sampleImage.getContext("2d").getImageData(0, 0, IMAGE_SIZE, IMAGE_SIZE);
-    // imageData.data is a uint8 raw pixel array
-
-
-    for (const MODIFIER of IMAGE_MODIFIERS) {
-        let frame = document.createElement("div");
-        frame.classList.add("canvas-frame");
-        frame.style.width = `${IMAGE_SIZE}px`;
-        frame.style.height = `${IMAGE_SIZE}px`;
-        frame.style.minWidth = `${IMAGE_SIZE}px`;
-        frame.style.minHeight = `${IMAGE_SIZE}px`;
-
-        gallery.append(frame);
-
-        const canvas = document.createElement("canvas");
-        const ctx = canvas.getContext("2d");
-        canvas.width = IMAGE_SIZE;
-        canvas.height = IMAGE_SIZE;
-        ctx.putImageData(MODIFIER.fn(imageData), 0, 0);
-        frame.append(canvas);
-    }
-
-    sampleContainer.append(gallery);
+    return sampleImage.getContext("2d").getImageData(0, 0, IMAGE_SIZE, IMAGE_SIZE);
 }
 
+function buildGallery(imageData, galleryTitle) {
+    let sectionContainer = document.getElementById(`${galleryTitle}-container`);
+
+    let gallery = document.createElement("div");
+    gallery.id = `${galleryTitle}Gallery`;
+    gallery.classList.add("gallery");
+
+    for (const MODIFIER of IMAGE_MODIFIERS) {
+        let container = document.createElement("div");
+        container.classList.add("imagebox");
+        gallery.append(container);
+
+        let title = document.createElement("h3")
+        title.innerText = MODIFIER.title;
+        container.append(title);
+
+        let frame = document.createElement("div");
+        frame.classList.add("canvas-frame");
+        container.append(frame);
+
+        const canvas = document.createElement("canvas");
+        canvas.id = `canvas-${galleryTitle}-${MODIFIER.title}`;
+        canvas.width = IMAGE_SIZE;
+        canvas.height = IMAGE_SIZE;
+        const ctx = canvas.getContext("2d");
+        console.log(JSON.stringify(MODIFIER.fn(imageData)));
+        ctx.putImageData(MODIFIER.fn(imageData), 0, 0);
+        frame.append(canvas);
+
+        // TODO: SETUP RESULT HTML HERE
+
+        // Update Listener
+        canvas.dataset.base = `canvas-${galleryTitle}-Original`;
+        canvas.dataset.control = MODIFIER.control;
+        canvas.addEventListener('controlChanged', (event) => {
+            if (event.target.dataset.control === event.detail.control)
+            {
+                const MODIFIER = IMAGE_MODIFIERS.find(x => x.control === event.target.dataset.control);
+                if (MODIFIER) {
+
+                    const canvas = document.getElementById(event.target.dataset.base);
+                    if (!canvas) {
+                        console.warn("Failed to retrieve baseline image for controls update.");
+                        return;
+                    }
+
+                    const baseImageData = canvas.getContext("2d").getImageData(0, 0, IMAGE_SIZE, IMAGE_SIZE);
+                    const ctx = event.target.getContext("2d");
+                    ctx.putImageData(MODIFIER.fn(baseImageData), 0, 0);
+                    // TODO: Update the image statistics table above
+                }
+            }
+        });
 
 
 
+        if (MODIFIER.showDownload === true) {
+            const downloadButton = document.createElement("button");
+            downloadButton.innerText = "Download";
+            downloadButton.type = "button";
+            downloadButton.dataset.downloadTarget = `${galleryTitle}-${MODIFIER.title}`;
+            downloadButton.addEventListener('click', (event) => {
+                downloadImage(event.currentTarget.dataset["downloadTarget"]);
+            });
+            container.append(downloadButton);
+        }
+    }
+
+    sectionContainer.append(gallery);
+}
+
+function downloadImage(targetId) {
+    const canvas = document.getElementById(targetId);
+    if (canvas)
+    {
+        const ctx = canvas.getContext("2d");
+        ctx.getImageData(0, 0, IMAGE_SIZE, IMAGE_SIZE)
+        // TODO: Finish download
+    }
+    // console.log("Download: " + JSON.stringify(imageData));
+}
 
 async function main() {
 
     setupControls();
 
-    // buildGallery();
+    const sampleImage = getSampleImage();
+    buildGallery(sampleImage, "sample");
 
-    /*
-    let sampleFrame = document.getElementById("sample-frame");
-    sampleCanvas = createSampleImage(IMAGE_SIZE);
-    sampleFrame.append(sampleCanvas);
-    sampleFrame.append(sampleCanvas);
-
-    let personalFrame = document.getElementById("personal-frame");
-    */
-
-
+    const personalImage = await getPersonalImage();
+    if (typeof(personalImage) !== "string") {
+        buildGallery(personalImage, "personal");
+    } else {
+        let sectionContainer = document.getElementById(`personal-container`);
+        let errorText = document.createElement("p");
+        errorText.innerText = personalImage;
+        sectionContainer.append(errorText);
+    }
 }
 
 main();
--- src/lib/image/blockAverage.js
+++ src/lib/image/blockAverage.js
@@ -10,5 +10,60 @@
  * @returns {ImageData}
  */
 export function averageBlocks(_imageData, _blockSize) {
-  throw new Error("averageBlocks: not implemented");
+
+  if (_blockSize === 0)
+    return _imageData;
+
+  // JPEG uses 8x8 blocks, edges by default are padded by extending edges
+  // const numBlocks = Math.ceil(_imageData.width / _blockSize);
+
+  // Calculate block position (X, Y)
+  // Sum using line by line,
+  let blockSums = []
+
+  for (let y = 0; y < _imageData.height; y++) {
+
+  }
+
+  /*
+  let startPos = {
+    x: (numBlocks % 2 !== 0) ? Math.ceil(_blockSize/2) : 0,
+    y: (numBlocks % 2 !== 0) ? -Math.ceil(_blockSize/2) : 0
+  };
+
+  for (let i = 0; i < numBlocks**2; i++) {
+    let targetPos = {
+      x: startPos.x + i % numBlocks,
+      y: startPos.y + Math.floor(i / numBlocks)
+    }
+
+    const colorSum = [0, 0, 0, 0] // RGBA
+    for(let ix = 0; ix < _blockSize**2; i++) {
+      const index = ((targetPos.x + ix) + (targetPos.y + ix % _blockSize) * _imageData.width) * 4;
+      colorSum[0] += _imageData[index]
+      colorSum[1] += _imageData[index+1]
+      colorSum[2] += _imageData[index+2]
+      colorSum[3] += _imageData[index+3]
+    }
+
+    for(let ix = 0; ix < _blockSize**2; i++) {
+      const index = ((targetPos.x + ix) + (targetPos.y + ix % _blockSize) * _imageData.width) * 4;
+      _imageData[index] = colorSum[0]
+      _imageData[index+1] = colorSum[1]
+      _imageData[index+2] = colorSum[2]
+      _imageData[index+3] = colorSum[3]
+    }
+  }*/
+
+
+
+
+
+
+
+
+
+
+
+  return _imageData;
 }
